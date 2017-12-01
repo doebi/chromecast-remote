a6526678a4814918ff934f8e7e96630de3c1e750
Christoph Döberl <christoph@doeberl.at>
2017-12-01 22:15:28 +0100

    decreased quality

--- remote.js
+++ remote.js
@@ -4,10 +4,10 @@ let key;
 let state = null;
 
 let streams = {
-    "orf1": { name: "ORF eins", url: "http://apasfiisl.apa.at/ipad/orf1_q8c/orf.sdp/playlist.m3u8" },
-    "orf2": { name: "ORF2", url: "http://apasfiisl.apa.at/ipad/orf2_q8c/orf.sdp/playlist.m3u8" },
-    "orf3": { name: "ORF III Kultur und Information", url: "http://apasfiisl.apa.at/ipad/orf3_q8c/orf.sdp/playlist.m3u8" },
-    "orfs": { name: "ORF Sport+", url: "http://apasfiisl.apa.at/ipad/orfs_q8c/orf.sdp/playlist.m3u8" },
+    "orf1": { name: "ORF eins", url: "http://apasfiisl.apa.at/ipad/orf1_q6a/orf.sdp/playlist.m3u8" },
+    "orf2": { name: "ORF2", url: "http://apasfiisl.apa.at/ipad/orf2_q6a/orf.sdp/playlist.m3u8" },
+    "orf3": { name: "ORF III Kultur und Information", url: "http://apasfiisl.apa.at/ipad/orf3_q6a/orf.sdp/playlist.m3u8" },
+    "orfs": { name: "ORF Sport+", url: "http://apasfiisl.apa.at/ipad/orfs_q6a/orf.sdp/playlist.m3u8" },
     "br": { name: "BR: Bayerischer Rundfunk", url: "http://brlive-lh.akamaihd.net/i/bfssued_worldwide@119891/master.m3u8" },
     "3sat": { name: "3sat", url: "http://zdf0910-lh.akamaihd.net/i/dach10_v1@392872/master.m3u8" },
     "ard": { name: "Das Erste", url: "http://live-lh.daserste.de/i/daserste_de@91204/master.m3u8" },
